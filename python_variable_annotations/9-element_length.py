d75f9fcb998c8f9d7c6e0927f9f3bdb8503a8e8b
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-11-23 18:59:48 +0000

    9-element_length.py

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module that defines element_length function with type annotations.
+"""
+
+from typing import Iterable, Sequence, List, Tuple
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Return a list of tuples with each element from lst and its length.
+    """
+    return [(i, len(i)) for i in lst]
