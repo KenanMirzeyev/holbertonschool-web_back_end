ed59e3c2cf2d4244bfedee8e4efdd81633e4d22c
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-11-23 19:18:32 +0000

    1-concurrent_coroutines.py

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -4,7 +4,7 @@ Test file for printing the correct output of the wait_n coroutine
 '''
 
 import asyncio
-from 0-basic_async_syntax import wait_random
+from 0_basic_async_syntax import wait_random
 from typing import List
 
 
