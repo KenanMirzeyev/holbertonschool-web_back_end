eab35ab1e465405f3c901c3bf0915f67c0350323
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-12-07 13:38:43 +0000

    4-tasks.py

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 """
-A
+Run multiple task_wait_random tasks.
 """
+
 import asyncio
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int):
     """
-    A
+    Run task_wait_random n times and return sorted delays.
     """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*tasks)
