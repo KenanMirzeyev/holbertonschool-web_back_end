fd691d18dbb11dbf4355f44be2760698aacb3065
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2026-01-01 17:43:10 +0000

    4-pricing.js

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,34 +1,34 @@
-import Currency from './3-currency'
+import Currency from './3-currency';
 
 class Pricing {
-    constructor(amount, currency) {
-        this.amount = amount;
-        this.currency = currency;
-    }
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
 
-    get amount() {
-        return this._amount;
-    }
+  get amount() {
+    return this._amount;
+  }
 
-    set amount(amount) {
-        if (typeof amount === 'number') this._amount = amount;
-    }
+  set amount(amount) {
+    if (typeof amount === 'number') this._amount = amount;
+  }
 
-    get currency() {
-        return this._currency;
-    }
+  get currency() {
+    return this._currency;
+  }
 
-    set currency(currency) {
-        if (typeof currency === 'number') this._currency = currency;
-    }
+  set currency(currency) {
+    if (currency instanceof Currency) this._currency = currency;
+  }
 
-    displayFullPrice() {
-        return `${this.amount} ${this.currency.name} (${this.currency.code})`;
-    }
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
 
-    static convertPrice(amount, conversionRate) {
-        return amount * conversionRate;
-    }
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }
 
 export default Pricing;
