2af6a453dd842648ddf75de1db89a753930d1d27
KenanMirzeyev <aida-mirzoyeva.87@mail.ru>
2026-01-11 14:20:45 +0000

    8-clean_set.js

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,10 @@
+export default function cleanSet(set, startString) {
+  if (!startString || !set || !(set instanceof Set)) {
+    return '';
+  }
+
+  return [...set]
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
+    .join('-');
+}
